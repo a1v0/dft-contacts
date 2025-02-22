c9e2c14f5200907d5c3816ad9a74efd6844c4d21
a1v0 <avc285@gmail.com>
2025-02-22 15:21:56 +0000

    Refactor how types are used

--- dft_contacts/contacts/models.py
+++ dft_contacts/contacts/models.py
@@ -1,20 +1,24 @@
 from django.db import models
-from enum import Enum
-
-
-class TypeEnum(Enum):
-    FAMILY = 1, "Family"
-    FRIENDS = 2, "Friends"
-    WORK = 3, "Work"
-    TRADE = 4, "Trade"
-    OTHER = 5, "Other"
 
 
 class Contact(models.Model):
+    CONTACT_TYPES = [
+        ("1", "Family"),
+        ("2", "Friends"),
+        ("3", "Work"),
+        ("4", "Trade"),
+        ("5", "Other"),
+    ]
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.CharField(max_length=100)
     mobile = models.CharField(max_length=20)
-    type = models.IntegerField(
-        choices=[(type.value[0], type.value[1]) for type in TypeEnum]
+    contact_type = models.CharField(
+        max_length=2,
+        choices=CONTACT_TYPES,
     )
+
+    def type_verbose(self):
+        # This method exists because I couldn't get get_contact_type_display to work in the template
+        return dict(Contact.CONTACT_TYPES)[self.contact_type]
