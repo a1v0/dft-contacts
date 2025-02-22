a392b3e438c51730a82dbf8346a03d3f3b448b1b
a1v0 <avc285@gmail.com>
2025-02-22 15:22:08 +0000

    Send all contacts into view

--- dft_contacts/contacts/views.py
+++ dft_contacts/contacts/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Contact
 
 
 def contacts(request):
+    all_contacts = Contact.objects.all()
     template = loader.get_template("contacts_template.html")
-    return HttpResponse(template.render())
+    context = {"all_contacts": all_contacts}
+
+    return HttpResponse(template.render(context, request))
