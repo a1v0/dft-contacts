d7cbaa8a8f8615875dc3adaeb0c3191f69476b31
a1v0 <avc285@gmail.com>
2025-02-22 17:26:43 +0000

    Create `/` route

--- dft_contacts/contacts/urls.py
+++ dft_contacts/contacts/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path(
+        "", views.contacts, name="contacts"
+    ),  # As soon as more than one single view is needed, this line can be deleted in favour of better routing
     path("contacts/", views.contacts, name="contacts"),
 ]
