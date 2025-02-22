e825fe8e0f47969c64ba2b7bfcbfab3613c31886
a1v0 <avc285@gmail.com>
2025-02-22 12:21:01 +0000

    Lead contacts view

--- dft_contacts/contacts/views.py
+++ dft_contacts/contacts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+
+def contacts(request):
+    template = loader.get_template("contacts_template.html")
+    return HttpResponse(template.render())
