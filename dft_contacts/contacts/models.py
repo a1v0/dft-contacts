f3ba814e1c72805992a27efece370a814e6bcea1
a1v0 <avc285@gmail.com>
2025-02-22 13:37:16 +0000

    Create `Contact` model

--- dft_contacts/contacts/models.py
+++ dft_contacts/contacts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from enum import Enum
 
-# Create your models here.
+
+class TypeEnum(Enum):
+    FAMILY = 1, "Family"
+    FRIENDS = 2, "Friends"
+    WORK = 3, "Work"
+    TRADE = 4, "Trade"
+    OTHER = 5, "Other"
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=20)
+    type = models.CharField(
+        max_length=1, choices=[(type.value[0], type.value[1]) for type in TypeEnum]
+    )
