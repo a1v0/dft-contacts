9101daf96bf75f2b5da57088178a6642588c005c
a1v0 <avc285@gmail.com>
2025-02-22 16:37:01 +0000

    Add form to view

--- dft_contacts/contacts/views.py
+++ dft_contacts/contacts/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Contact
+from forms import ContactForm
 
 
 def contacts(request):
     all_contacts = Contact.objects.all()
     template = loader.get_template("contacts_template.html")
-    context = {"all_contacts": all_contacts}
+    form = ContactForm()
+
+    context = {}
+    context["all_contacts"] = all_contacts
+    context["form"] = form
 
     return HttpResponse(template.render(context, request))
